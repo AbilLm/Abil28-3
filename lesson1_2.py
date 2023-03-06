d8f57a6d88da006667506268607154c0f620d1ea
GigaN1ga <loppupossum@gmail.com>
2023-03-06 18:01:24 +0600

    ghg

---
+++ lesson1_2.py
@@ -0,0 +1,9 @@
+class Kat:
+    tail = True
+    usiki = True
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+    def may(self):
+        print(self.name, 'may')
