626e3a200b398292d7a405c72d940990e30d6eef
GigaN1ga <loppupossum@gmail.com>
2023-03-08 17:37:07 +0600

    hw1

--- lesson1_2.py
+++
@@ -1,9 +0,0 @@
-class Kat:
-    tail = True
-    usiki = True
-    def __init__(self, name, age, color):
-        self.name = name
-        self.age = age
-        self.color = color
-    def may(self):
-        print(self.name, 'may')
